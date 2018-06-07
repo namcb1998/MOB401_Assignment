50a3792b8a5002732d6a6d87d420e9d3bd484379
namcb1998 <namcb1998@gmail.com>
2018-06-07 16:47:51 +0700

    linh oc cho

--- Assets/_Scripts/Player.cs
+++ Assets/_Scripts/Player.cs
@@ -4,93 +4,118 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-	[SerializeField] private float jumpForceAbs;
-	private float jumpForce;
-	private float freshJumpForce;
-	private Rigidbody2D _rigidbody2D;
+    [SerializeField] private float jumpForceAbs;
+    private float jumpForce;
+    private float freshJumpForce;
+    private Rigidbody2D _rigidbody2D;
 
-	private bool isGround;
-	private bool isFreshJump;
-	private bool canJump;
+    private bool isGround;
+    private bool isFreshJump;
+    private bool canJump;
 
-	private Animator _animator;
-	
-	// Use this for initialization
-	void Start ()
-	{
-		_rigidbody2D = GetComponent<Rigidbody2D>();
-		freshJumpForce = jumpForceAbs * 5f;
-		canJump = false;
-		jumpForce = jumpForceAbs;
-		_animator = GetComponent<Animator>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private Animator _animator;
 
-	private void FixedUpdate()
-	{
-		Jump();
-	}
+    private float lastTimePressedSpace;
+    [SerializeField] private AudioClip[] _audioClips;
+    // 0 jump , 1 superhero landing , 3 coin
 
+    // Use this for initialization
+    void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        freshJumpForce = jumpForceAbs * 4.7f;
+        canJump = false;
+        jumpForce = jumpForceAbs;
+        _animator = GetComponent<Animator>();
+        lastTimePressedSpace = Time.time;
+    }
 
-	private void OnTriggerEnter2D(Collider2D other)
-	{
-		isGround = true;
-		Debug.Log("OnTriggerEnter2D");
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        
 
-	private void OnTriggerStay2D(Collider2D other)
-	{
-		isGround = true;
-		Debug.Log("OnTriggerStay2D");
-	}
+    }
 
-	private void OnTriggerExit2D(Collider2D other)
-	{
-		isGround = false;
-		Debug.Log("OnTriggerExit2D");
-		_animator.SetBool("isRunning",false);
-	}
-
-	private void Jump()
-	{
-		if (Input.GetKey(KeyCode.Space)&&isFreshJump)
-		{
-			Debug.Log("fresh");
-			_rigidbody2D.AddForce(Vector2.up*freshJumpForce,ForceMode2D.Force);
-			isFreshJump = false;
-		}else if (Input.GetKey(KeyCode.Space)&&canJump)
-		{
-			_rigidbody2D.AddForce(Vector2.up*jumpForce,ForceMode2D.Force);
-		}
-
-		if (Input.GetKeyUp(KeyCode.Space)&&!isGround)
-		{
-			canJump = false;
-		}
-		
+    private void FixedUpdate()
+    {
+        Jump();
+    }
 
 
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Respawn")
+        {
+            isGround = true;
+           // GetComponent<AudioSource>().PlayOneShot(_audioClips[1]);
+        }
+    }
 
-		if (!isGround)
-		{
-			jumpForce -= 2f;
-			if (jumpForce<0)
-			{
-				jumpForce = 0;
-			}
-		}
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Respawn")
+        {
+            isGround = true;
+        }
+    }
 
-		if (isGround)
-		{
-			_animator.SetBool("isRunning",true);
-			isFreshJump = true;
-			jumpForce = jumpForceAbs;
-			canJump = true;
-		}
-	}
-	//link oc cho
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Respawn")
+        {
+            isGround = false;
+            _animator.SetBool("isRunning", false);
+        }
+    }
+
+    private void Jump()
+    {
+        if (Input.GetKey(KeyCode.Space)&&isFreshJump)
+        {
+            
+        }
+        if (Input.GetKey(KeyCode.Space) && isFreshJump)
+        {
+            if (lastTimePressedSpace<Time.time-0.3f)
+            {
+                isFreshJump = false;
+                lastTimePressedSpace = Time.time;
+                GetComponent<AudioSource>().PlayOneShot(_audioClips[0]);
+                _rigidbody2D.AddForce(Vector2.up * freshJumpForce*4, ForceMode2D.Force);
+
+            }
+            
+
+        }
+        else if (Input.GetKey(KeyCode.Space) && canJump)
+        {
+            _rigidbody2D.AddForce(Vector2.up * jumpForce, ForceMode2D.Force);
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space) && !isGround)
+        {
+            canJump = false;
+        }
+
+
+        if (!isGround)
+        {
+            jumpForce -= 2f;
+            if (jumpForce < 0)
+            {
+                jumpForce = 0;
+            }
+        }
+
+        if (isGround)
+        {
+            _animator.SetBool("isRunning", true);
+            isFreshJump = true;
+            jumpForce = jumpForceAbs;
+            canJump = true;
+        }
+    }
+    
+    //link oc cho
 }
